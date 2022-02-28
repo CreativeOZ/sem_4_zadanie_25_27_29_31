6a9fa692c3936baf8e7ac3cc34a63d3380b3e6aa
CreativeOZ <zagriaoleg@gmail.com>
2022-02-28 16:14:15 +0300

    Написать программу вычисления произведения чисел от 1 до N.

--- zadanie_29/Program.cs
+++ zadanie_29/Program.cs
@@ -3,9 +3,21 @@ Console.Clear();
 
 Console.Write("Ввести число: ");
 int A = int.Parse(Console.ReadLine());
-int B = 1;
+/* int B = 1;
 for (int i = 1; i <= A; i++)
 {
     B = B * i;
 }
-Console.WriteLine("Произведение чисел:" + B);
+Console.WriteLine("Произведение чисел:" + B); */
+
+int f = Factorial(A);
+int Factorial(int A)
+{
+    int B = 1;
+    for (int i = 1; i <= A; i++)
+    {
+        B = B * i;
+    }
+    return B;
+}
+Console.WriteLine("Факториал числа" + f);
